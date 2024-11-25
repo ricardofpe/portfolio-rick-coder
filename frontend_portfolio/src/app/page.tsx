3581cb3e0824bcc0959631169e2a398cd973aea6
ricardofpe <ricardofonseca_1@hotmail.com>
2024-11-25 20:42:36 -0300

    Add component about

--- frontend_portfolio/src/app/page.tsx
+++ frontend_portfolio/src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/About";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="container">
       <Header />
+      <About />
     </div>
   );
 }
---
+++ frontend_portfolio/src/components/About.tsx
@@ -0,0 +1,35 @@
+export default function About() {
+  return (
+    <section className="flex flex-col mt-28 gap-6">
+      <h6 className="font-semibold">ABOUT ME</h6>
+
+      <p className="text-gray-400">
+        I'm a passionate full-stack developer dedicated to crafting accessible
+        and visually stunning user interfaces that seamlessly blend thoughtful
+        design with robust engineering. My ideal projects lie at the
+        intersection of design and development, creating experiences that are
+        not only aesthetically pleasing but also meticulously built for optimal
+        performance and usability.
+      </p>
+
+      <p className="text-gray-400">
+        My skillset spans front-end, back-end, and mobile development. On the
+        front-end, I'm proficient in React, Next.js, TypeScript, and JavaScript,
+        leveraging styling frameworks like Tailwind CSS and Styled Components to
+        build modern and responsive interfaces. For the back-end, I utilize
+        Node.js and NestJS to construct robust and scalable APIs. My mobile
+        development experience is primarily with React Native. MongoDB is my
+        preferred database solution for many projects. I hold a Bachelor's
+        degree in Systems Analysis and Development and have one year of
+        professional experience in the field.
+      </p>
+
+      <p className="text-gray-400">
+        Throughout my career, I've focused on developing integration systems
+        designed to simplify user workflows and automate repetitive processes,
+        significantly improving efficiency and user experience. This experience
+        has been gained across a variety of projects and contexts.
+      </p>
+    </section>
+  );
+}
