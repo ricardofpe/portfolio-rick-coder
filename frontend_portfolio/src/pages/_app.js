c8f30d0a385183b55bbf5a78b12aca7892008e44
ricardofpe <ricardofonseca_1@hotmail.com>
2024-11-24 21:01:39 -0300

    Add font poppins

---
+++ frontend_portfolio/src/pages/_app.js
@@ -0,0 +1,16 @@
+
+import Head from 'next/head';
+import '../app/global.css';
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,100&display=swap" rel="stylesheet" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
