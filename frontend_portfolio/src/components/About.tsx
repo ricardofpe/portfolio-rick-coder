623741b41d643531c5f4429256b4fec9507a595d
ricardofpe <ricardofonseca_1@hotmail.com>
2024-11-28 20:37:21 -0300

    Correct error to deploy

--- frontend_portfolio/src/components/About.tsx
+++ frontend_portfolio/src/components/About.tsx
@@ -20,9 +20,9 @@ export default function About() {
       </h6>
 
       <p className="text-gray-300 text-sm md:text-base lg:text-lg leading-relaxed">
-        I'm a passionate full-stack developer dedicated to crafting accessible
-        and visually stunning user interfaces that seamlessly blend thoughtful
-        design with robust engineering. My ideal projects lie at the
+        I&apos;m a passionate full-stack developer dedicated to crafting
+        accessible and visually stunning user interfaces that seamlessly blend
+        thoughtful design with robust engineering. My ideal projects lie at the
         intersection of design and development, creating experiences that are
         not only aesthetically pleasing but also meticulously built for optimal
         performance and usability.
@@ -30,21 +30,21 @@ export default function About() {
 
       <p className="text-gray-300 text-sm md:text-base lg:text-lg leading-relaxed">
         My skillset spans front-end, back-end, and mobile development. On the
-        front-end, I'm proficient in React, Next.js, TypeScript, and JavaScript,
-        leveraging styling frameworks like Tailwind CSS and Styled Components to
-        build modern and responsive interfaces. For the back-end, I utilize
-        Node.js and NestJS to construct robust and scalable APIs. My mobile
-        development experience is primarily with React Native. MongoDB is my
-        preferred database solution for many projects. I hold a Graduation in
+        front-end, I&apos;m proficient in React, Next.js, TypeScript, and
+        JavaScript, leveraging styling frameworks like Tailwind CSS and Styled
+        Components to build modern and responsive interfaces. For the back-end,
+        I utilize Node.js and NestJS to construct robust and scalable APIs. My
+        mobile development experience is primarily with React Native. MongoDB is
+        my preferred database solution for many projects. I hold a Graduation in
         Systems Analysis and Development and have one year of professional
         experience in the field.
       </p>
 
       <p className="text-gray-300 text-sm md:text-base lg:text-lg leading-relaxed">
-        Throughout my career, I've focused on developing integration systems
-        designed to simplify user workflows and automate repetitive processes,
-        significantly improving efficiency and user experience. This experience
-        has been gained across a variety of projects and contexts.
+        Throughout my career, I&apos;ve focused on developing integration
+        systems designed to simplify user workflows and automate repetitive
+        processes, significantly improving efficiency and user experience. This
+        experience has been gained across a variety of projects and contexts.
       </p>
     </section>
   );
