83c9f5b352c8bd305ab1a7ab45bcdc7f782926dc
ricardofpe <ricardofonseca_1@hotmail.com>
2024-11-28 21:02:52 -0300

    Correct title and description in layout.js

--- frontend_portfolio/src/app/layout.tsx
+++ frontend_portfolio/src/app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "portfolio",
-  description: "Rick Code portfolio",
+  title: "Rick Code",
+  description: "portfolio",
 };
 
 export default function RootLayout({
